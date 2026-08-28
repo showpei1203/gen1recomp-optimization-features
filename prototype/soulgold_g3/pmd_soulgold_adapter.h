fb12001df889f9c99cd999a13bec53448f34fe1b
showpei1203 <showpei1203@gmail.com>
2026-08-28 17:28:06 +0800

    G3R3 split PMD loaded-buffer and template ownership

--- prototype/soulgold_g3/pmd_soulgold_adapter.h
+++ prototype/soulgold_g3/pmd_soulgold_adapter.h
@@ -5,6 +5,7 @@
 
 void PmdSoulGold_Init(void);
 void PmdSoulGold_Reset(void);
-bool32 PmdSoulGold_PrimeBodyFrame(u8 battler, const struct PmdGbaFrame *frame);
+bool32 PmdSoulGold_PrimeLoadedBody(u8 battler, const struct PmdGbaFrame *frame);
+bool32 PmdSoulGold_PrimeTemplateBody(u8 battler, const struct PmdGbaFrame *frame);
 
 #endif // GUARD_PMD_SOULGOLD_ADAPTER_H
