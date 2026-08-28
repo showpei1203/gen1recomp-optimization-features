3396d22ca97bdffa355ab194fad9fbcc3cb5eb57
showpei1203 <showpei1203@gmail.com>
2026-08-29 07:41:48 +0800

    Add Showdown SoulGold adapter interface

---
+++ prototype/soulgold_showdown_s1/showdown_soulgold_adapter.h
@@ -0,0 +1,7 @@
+#ifndef GUARD_SHOWDOWN_SOULGOLD_ADAPTER_H
+#define GUARD_SHOWDOWN_SOULGOLD_ADAPTER_H
+
+void ShowdownSoulGold_Init(void);
+void ShowdownSoulGold_Reset(void);
+
+#endif // GUARD_SHOWDOWN_SOULGOLD_ADAPTER_H
