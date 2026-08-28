c4a3d31692ea25d6219664c717d1e1543ad2f81f
showpei1203 <showpei1203@gmail.com>
2026-08-28 15:12:30 +0800

    G2: add Rich Ambient prototype header

---
+++ prototype/soulgold_g2/pmd_soulgold_prototype.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_PMD_SOULGOLD_PROTOTYPE_H
+#define GUARD_PMD_SOULGOLD_PROTOTYPE_H
+
+void PmdSoulGoldPrototype_Init(void);
+void PmdSoulGoldPrototype_Tick(void);
+void PmdSoulGoldPrototype_Reset(void);
+
+#endif // GUARD_PMD_SOULGOLD_PROTOTYPE_H
