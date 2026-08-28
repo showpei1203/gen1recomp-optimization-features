2a73eaa654eda9f4dafebce2718db868920ec1bd
showpei1203 <showpei1203@gmail.com>
2026-08-28 14:12:25 +0800

    G1: add SoulGold prototype lifecycle API

---
+++ prototype/soulgold_g1/pmd_soulgold_prototype.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_PMD_SOULGOLD_PROTOTYPE_H
+#define GUARD_PMD_SOULGOLD_PROTOTYPE_H
+
+#include "global.h"
+
+void PmdSoulGoldPrototype_Init(void);
+void PmdSoulGoldPrototype_Tick(void);
+void PmdSoulGoldPrototype_Reset(void);
+void PmdSoulGoldPrototype_OnBattlerSpriteReady(u8 battler);
+
+#endif // GUARD_PMD_SOULGOLD_PROTOTYPE_H
