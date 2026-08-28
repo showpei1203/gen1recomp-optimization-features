1e8b4247be612e9c4497970e2a3e37ba0935cad5
showpei1203 <showpei1203@gmail.com>
2026-08-28 14:16:00 +0800

    G1: rename sprite lifecycle hook as generation invalidation

--- prototype/soulgold_g1/pmd_soulgold_prototype.h
+++ prototype/soulgold_g1/pmd_soulgold_prototype.h
@@ -6,6 +6,6 @@
 void PmdSoulGoldPrototype_Init(void);
 void PmdSoulGoldPrototype_Tick(void);
 void PmdSoulGoldPrototype_Reset(void);
-void PmdSoulGoldPrototype_OnBattlerSpriteReady(u8 battler);
+void PmdSoulGoldPrototype_InvalidateBattlerSpriteGeneration(u8 battler);
 
 #endif // GUARD_PMD_SOULGOLD_PROTOTYPE_H
