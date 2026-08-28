6666755ec914edebc49bd35001553f673e780b78
showpei1203 <showpei1203@gmail.com>
2026-08-28 18:17:28 +0800

    G3R4 expose post-create body prime

---
+++ prototype/soulgold_g3r4/pmd_soulgold_prototype.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_PMD_SOULGOLD_PROTOTYPE_H
+#define GUARD_PMD_SOULGOLD_PROTOTYPE_H
+
+void PmdSoulGoldPrototype_Init(void);
+void PmdSoulGoldPrototype_Tick(void);
+void PmdSoulGoldPrototype_Reset(void);
+void PmdSoulGoldPrototype_PrimeLoadedBattlerBody(u8 battler);
+void PmdSoulGoldPrototype_PrimeTemplateBody(u8 battler, u16 species);
+void PmdSoulGoldPrototype_PrimeCreatedSpriteBody(u8 battler, u16 species);
+
+#endif // GUARD_PMD_SOULGOLD_PROTOTYPE_H
