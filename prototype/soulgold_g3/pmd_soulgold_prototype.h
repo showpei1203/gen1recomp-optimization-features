25d4d3ff0ec0837d1fb1e6cb4b91af7a9aa82071
showpei1203 <showpei1203@gmail.com>
2026-08-28 17:28:44 +0800

    G3R3 expose species-aware PMD prime hooks

--- prototype/soulgold_g3/pmd_soulgold_prototype.h
+++ prototype/soulgold_g3/pmd_soulgold_prototype.h
@@ -4,6 +4,7 @@
 void PmdSoulGoldPrototype_Init(void);
 void PmdSoulGoldPrototype_Tick(void);
 void PmdSoulGoldPrototype_Reset(void);
-void PmdSoulGoldPrototype_PrimeBattlerBody(u8 battler);
+void PmdSoulGoldPrototype_PrimeLoadedBattlerBody(u8 battler);
+void PmdSoulGoldPrototype_PrimeTemplateBody(u8 battler, u16 species);
 
 #endif // GUARD_PMD_SOULGOLD_PROTOTYPE_H
