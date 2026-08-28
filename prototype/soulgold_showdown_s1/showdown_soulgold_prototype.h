d25f4d3f03ec64e5d921972dcff5280e9afef6c7
showpei1203 <showpei1203@gmail.com>
2026-08-29 07:42:04 +0800

    Add Showdown SoulGold prototype interface

---
+++ prototype/soulgold_showdown_s1/showdown_soulgold_prototype.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_SHOWDOWN_SOULGOLD_PROTOTYPE_H
+#define GUARD_SHOWDOWN_SOULGOLD_PROTOTYPE_H
+
+void ShowdownSoulGoldPrototype_Init(void);
+void ShowdownSoulGoldPrototype_Tick(void);
+void ShowdownSoulGoldPrototype_Reset(void);
+
+#endif // GUARD_SHOWDOWN_SOULGOLD_PROTOTYPE_H
