7d055dacb8f6aed87683f48d009492c290c7465e
showpei1203 <showpei1203@gmail.com>
2026-08-29 08:02:28 +0800

    Expose temporary Showdown S1B battle harness

--- prototype/soulgold_showdown_s1/showdown_soulgold_prototype.h
+++ prototype/soulgold_showdown_s1/showdown_soulgold_prototype.h
@@ -4,5 +4,6 @@
 void ShowdownSoulGoldPrototype_Init(void);
 void ShowdownSoulGoldPrototype_Tick(void);
 void ShowdownSoulGoldPrototype_Reset(void);
+bool32 ShowdownSoulGoldPrototype_TryStartTestBattle(u16 newKeys);
 
 #endif // GUARD_SHOWDOWN_SOULGOLD_PROTOTYPE_H
