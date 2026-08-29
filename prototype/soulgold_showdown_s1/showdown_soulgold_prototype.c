eb8e8ccc919eaf031a9f65bd46840ee95364ba1b
showpei1203 <showpei1203@gmail.com>
2026-08-29 08:02:41 +0800

    Bind Sprigatito and Marill plus temporary B battle harness

--- prototype/soulgold_showdown_s1/showdown_soulgold_prototype.c
+++ prototype/soulgold_showdown_s1/showdown_soulgold_prototype.c
@@ -1,14 +1,17 @@
 #include "global.h"
 #include "battle.h"
+#include "battle_setup.h"
 #include "battle_util.h"
+#include "pokemon.h"
+#include "script.h"
 #include "sprite.h"
 #include "constants/species.h"
 #include "showdown_gba_runtime.h"
 #include "showdown_soulgold_adapter.h"
 #include "showdown_soulgold_prototype.h"
 
-extern const struct ShowdownGbaAction gShowdownCyndaquilBackIdleAction;
-extern const struct ShowdownGbaAction gShowdownCyndaquilFrontIdleAction;
+extern const struct ShowdownGbaAction gShowdownSprigatitoBackIdleAction;
+extern const struct ShowdownGbaAction gShowdownMarillFrontIdleAction;
 
 static bool8 sBound[SHOWDOWN_GBA_MAX_BATTLERS];
 static u8 sBoundSpriteId[SHOWDOWN_GBA_MAX_BATTLERS];
@@ -41,7 +44,7 @@ void ShowdownSoulGoldPrototype_Tick(void)
 
     for (battler = 0; battler < SHOWDOWN_GBA_MAX_BATTLERS; battler++)
     {
-        const struct ShowdownGbaAction *action;
+        const struct ShowdownGbaAction *action = NULL;
         u16 species;
         u8 spriteId;
 
@@ -54,7 +57,12 @@ void ShowdownSoulGoldPrototype_Tick(void)
         species = GetBattlerVisualSpecies(battler);
         spriteId = gBattlerSpriteIds[battler];
 
-        if (species != SPECIES_CYNDAQUIL)
+        if (GetBattlerSide(battler) == B_SIDE_PLAYER && species == SPECIES_SPRIGATITO)
+            action = &gShowdownSprigatitoBackIdleAction;
+        else if (GetBattlerSide(battler) == B_SIDE_OPPONENT && species == SPECIES_MARILL)
+            action = &gShowdownMarillFrontIdleAction;
+
+        if (action == NULL)
         {
             ClearBinding(battler);
             continue;
@@ -65,10 +73,6 @@ void ShowdownSoulGoldPrototype_Tick(void)
 
         if (!sBound[battler])
         {
-            action = GetBattlerSide(battler) == B_SIDE_PLAYER
-                ? &gShowdownCyndaquilBackIdleAction
-                : &gShowdownCyndaquilFrontIdleAction;
-
             if (ShowdownGbaRuntime_Bind(battler, action))
             {
                 sBound[battler] = TRUE;
@@ -87,4 +91,29 @@ void ShowdownSoulGoldPrototype_Reset(void)
     for (battler = 0; battler < SHOWDOWN_GBA_MAX_BATTLERS; battler++)
         ClearBinding(battler);
     ShowdownSoulGold_Reset();
+}
+
+bool32 ShowdownSoulGoldPrototype_TryStartTestBattle(u16 newKeys)
+{
+    u16 leadSpecies;
+    u8 leadLevel;
+
+    if (!(newKeys & B_BUTTON))
+        return FALSE;
+    if (ArePlayerFieldControlsLocked() || ScriptContext_IsEnabled())
+        return FALSE;
+
+    leadSpecies = GetMonData(&gPlayerParty[0], MON_DATA_SPECIES);
+    if (leadSpecies != SPECIES_SPRIGATITO)
+        return FALSE;
+
+    leadLevel = GetMonData(&gPlayerParty[0], MON_DATA_LEVEL);
+    if (leadLevel == 0 || leadLevel > MAX_LEVEL)
+        leadLevel = 5;
+
+    ZeroEnemyPartyMons();
+    CreateRandomMon(&gEnemyParty[0], SPECIES_MARILL, leadLevel);
+    gEnemyPartyCount = 1;
+    DoStandardWildBattle_Debug();
+    return TRUE;
 }
